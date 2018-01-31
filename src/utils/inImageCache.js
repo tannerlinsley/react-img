d88b03beca03cfcfe87982a7391bc040c32811c1
hmeissner <hugomeissner@icloud.com>
2018-01-31 21:04:20 +0100

    simple image cache

---
+++ src/utils/inImageCache.js
@@ -0,0 +1,14 @@
+// Cache if we've intersected an image before so we don't
+// lazy-load & fade in on subsequent mounts.
+const imageCache = {}
+const inImageCache = ({ handle }, shouldCache) => {
+  if (imageCache[handle]) {
+    return true
+  }
+  if (shouldCache) {
+    imageCache[handle] = true
+  }
+  return false
+}
+
+export default inImageCache
