43abe41222ed86b2cb6aa66be2116f985d66fef2
hmeissner <hugomeissner@icloud.com>
2018-01-31 21:04:33 +0100

    webp support check

---
+++ src/utils/isWebpSupported.js
@@ -0,0 +1,18 @@
+// check webp support
+let isWebpSupportedCache = null
+const isWebpSupported = () => {
+  if (isWebpSupportedCache !== null) {
+    return isWebpSupportedCache
+  }
+
+  const elem =
+    typeof window !== `undefined` ? window.document.createElement(`canvas`) : {}
+  if (elem.getContext && elem.getContext(`2d`)) {
+    isWebpSupportedCache =
+      elem.toDataURL(`image/webp`).indexOf(`data:image/webp`) === 0
+    return isWebpSupportedCache
+  }
+  return false
+}
+
+export default isWebpSupported
